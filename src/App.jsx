e17cb7e8b30e167b0351962ec52eb0ed06cb095e
Umar khan <umeralikhan7864@gmail.com>
2025-02-01 19:45:34 +0500

    React-Meta

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,23 @@ import Project from "./Components/Project.jsx";
 import Technologies from "./Components/Technologies.jsx";
 function App() {
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased  selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="fixed top-0 -z-10 h-full w-full">
-        <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+    <>
+      <div className="overflow-x-hidden text-neutral-300 antialiased  selection:bg-cyan-300 selection:text-cyan-900">
+        <div className="fixed top-0 -z-10 h-full w-full">
+          <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+        </div>
+
+        <div className="container mx-auto px-8">
+          <Navbar />
+          <Hero />
+          <About />
+          <Technologies />
+          <Experience />
+          <Project />
+          <Contact />
+        </div>
       </div>
-      <div className="container mx-auto px-8">
-        <Navbar />
-        <Hero />
-        <About />
-        <Technologies />
-        <Experience />
-        <Project />
-        <Contact />
-      </div>
-    </div>
+    </>
   );
 }
 
