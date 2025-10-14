92028d68dd6c91db25ed1014717d966ad448cb46
Umar Khan <143717285+thisisumarali@users.noreply.github.com>
2025-10-14 14:31:19 +0500

    padding

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
                 variants={container(0)}
                 initial="hidden"
                 animate="visible"
-                className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+                className="pb-12 lg:pb-14 xl:pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
               >
                 Umar Khan
               </motion.h1>
