a32bfa6b18738886e2614c9200d1f2685a90aa68
Umar khan <umeralikhan7864@gmail.com>
2025-11-24 21:46:24 +0500

    Udemy Certificate

--- src/Components/Certificates.jsx
+++ src/Components/Certificates.jsx
@@ -1,38 +1,47 @@
 import { motion } from "framer-motion";
 import certificateImg from "../assets/certificates.jpg";
+import certificateImg2 from "../assets/ucertficate.jpg";
 
 const Certificates = () => {
-    return (
-        <section className="border-b border-neutral-900 pb-16">
-            {/* Heading */}
-            <motion.h2
-                initial={{ opacity: 0, y: -40 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.6 }}
-                viewport={{ once: true }}
-                className="my-16 text-center text-4xl font-bold tracking-wide"
-            >
-                Certificates
-            </motion.h2>
+  return (
+    <section className="border-b border-neutral-900 pb-16">
+      {/* Heading */}
+      <motion.h2
+        initial={{ opacity: 0, y: -40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        viewport={{ once: true }}
+        className="my-16 text-center text-4xl font-bold tracking-wide"
+      >
+        Certificates
+      </motion.h2>
 
-            {/* Image */}
-            <motion.div
-                initial={{ opacity: 0, scale: 0.95 }}
-                whileInView={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.6, delay: 0.15 }}
-                viewport={{ once: true }}
-                className="flex justify-center"
-            >
-                <div className="w-full max-w-3xl rounded-xl overflow-hidden shadow-xl shadow-neutral-800/30">
-                    <img
-                        src={certificateImg}
-                        alt="Certificates"
-                        className="w-full h-auto object-cover"
-                    />
-                </div>
-            </motion.div>
-        </section>
-    );
+      {/* Image */}
+      <motion.div
+        initial={{ opacity: 0, scale: 0.95 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.6, delay: 0.15 }}
+        viewport={{ once: true }}
+        className="flex justify-center"
+      >
+        <div className="w-full max-w-4xl rounded-xl overflow-hidden shadow-xl shadow-neutral-800/30">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <img
+              src={certificateImg}
+              alt="Certificate 1"
+              className="w-full h-80  object-cover rounded-lg"
+            />
+
+            <img
+              src={certificateImg2}
+              alt="Certificate 2"
+              className="w-full h-80 object-cover rounded-lg"
+            />
+          </div>
+        </div>
+      </motion.div>
+    </section>
+  );
 };
 
 export default Certificates;
